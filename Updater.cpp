7d24692a6debb431e5f6e156100b1384867c7751
carl <newbility523@gmail.com>
2020-06-23 01:42:16 +0800

    add html style

--- Updater.cpp
+++ Updater.cpp
@@ -9,11 +9,72 @@
 #include "WebUpdate.h"
 #include <regex>
 #include <fstream>
+#include <codecvt>
+//#include <string>
 
 using namespace std;
 using std::string;
 using std::regex;
+using std::codecvt;
 using namespace std::experimental::filesystem;
+
+// config
+const char* srcDir = "E:\\Note";
+const char* targetDir = "E:\\Web";
+const char* cmdFormat = "showdown makehtml -i %s -o %s";
+const string cssPathStr = "config\\css\\github-markdown.css";
+const char* templatePath = "config\\Template.html";
+regex cssRegex("<!-- cssPath -->");
+regex bodyRegex("<!-- body -->");
+
+// global
+string htmlTemplate;
+string cssPath;
+
+int Init()
+{
+   /* cout << htmlPath.parent_path() << endl;
+    cout << absolute(htmlPath) << endl;*/
+    string htmlPath = absolute(templatePath).string();
+    if (!exists(htmlPath))
+    {
+        cout << "template not exists" << endl;
+        return -1;
+    }
+
+    cssPath = absolute(cssPathStr).string();
+    if (!exists(cssPath))
+    {
+        cout << "css not exists" << endl;
+        return -1;
+    }
+
+    ifstream ifileStream;
+    ifileStream.open(htmlPath, ios::out);
+    int length;
+    ifileStream.seekg(0, ios::end);
+    length = ifileStream.tellg();
+    char* buffer = new char[length + 1];
+    ifileStream.seekg(0, ios::beg);
+    ifileStream.read(buffer, length);
+    buffer[length] = '\0';
+    ifileStream.close();
+
+    htmlTemplate = buffer;
+    cout << htmlTemplate << endl;
+    cout << cssPath << endl;
+
+    return 0;
+}
+
+string FormatHtml(const char* buffer)
+{
+    string result = regex_replace(htmlTemplate, cssRegex, cssPath);
+    result = regex_replace(result, bodyRegex, buffer);
+
+    return result;
+}
+
 
 int CMDRunder(const char* cmd, char* result)
 {
@@ -37,10 +98,87 @@ int CMDRunder(const char* cmd, char* result)
     return 1;
 }
 
-void ConvertPath(path filePath)
+string ConvertPath(const char* buffer)
+{
+    //<p><a href="other/OtherMD.md#third">To other</a></p>
+    regex reg("(<a href=\".*?)\\.md(.*?\">)");
+    string newStr = regex_replace(buffer, reg, "$1.html$2");
+    return newStr;
+}
+
+void CreateCatalogue(vector<FileInfo>* infos)
+{
+    string catalogueStr = u8"# Catalogue \n\n";
+    const char* fmt = "* [%s](%s)\n";
+    for (vector<FileInfo>::const_iterator iter = infos->begin(); iter != infos->end(); ++iter)
+    {
+        FileInfo info = *iter;
+        char pathChar[300] = "";
+        info.FullPath(pathChar);
+        path path(pathChar);
+        if (path.extension() == ".md")
+        {
+            char item[100] = "";
+            //path.replace_extension();
+            string fullPath = path.u8string();
+            string relative = "E:\\Note\\";
+            fullPath = fullPath.substr(relative.length(), fullPath.length());
+            snprintf(item, sizeof(item), fmt, path.stem().u8string().c_str(), fullPath.c_str());
+            catalogueStr = catalogueStr + item;
+        }
+    }
+
+    //cout << "catalogueStr: " << catalogueStr << endl;
+
+    string fileStr = "E:\\Note\\Catalogue.md";
+    path catalogueInfoPath(fileStr);
+    fstream fstream;
+    fstream.open(fileStr, ios::trunc | ios::out);
+    if (fstream.good())
+    {
+        fstream.seekp(0, ios::beg);
+        /*    const char fileType[3] = { 0xEF, 0xBB , 0xBF };
+            fstream.write(fileType, 3);*/
+            /* const char* temp = u8"这是一行中文";
+             fstream.write(temp, strlen(temp));*/
+        fstream.write(catalogueStr.c_str(), catalogueStr.length());
+        fstream.close();
+
+        FileInfo catalogueInfo(catalogueInfoPath.parent_path().string().c_str(), catalogueInfoPath.filename().string().c_str());
+        infos->push_back(catalogueInfo);
+    }
+    else
+    {
+        cout << "bad" << endl;
+    }
+}
+
+int ClearTargetDirectory(const char* targetDir)
+{
+    path targetPath(targetDir);
+    if (!exists(targetPath))
+    {
+        create_directories(targetPath);
+    }
+
+    cout << "Are you sure remove all files in " << targetPath.string() << " ? (y/Y to continue) : ";
+    char isClear = getchar();
+    if (isClear == 'y' || isClear == 'Y')
+    {
+        cout << "\nAsk permission success." << endl;
+        int count = remove_all(targetPath);
+        cout << "Delete count: " << count << endl;
+    }
+    else
+    {
+        cout << "\nAsk permission fail, return." << endl;
+        return -1;
+    }
+}
+
+int FileFactor(path filePath)
 {
     string path = absolute(filePath).string();
-    //cout << path << endl;
 
     ifstream ifileStream;
     ifileStream.open(path, ios::out);
@@ -52,33 +190,40 @@ void ConvertPath(path filePath)
     ifileStream.read(buffer, length);
     buffer[length] = '\0';
     ifileStream.close();
-    //cout << "buffer: " << buffer << endl;
 
-    //<p><a href="other/OtherMD.md#third">To other</a></p>
-    regex reg("(<a href=\".*?)\\.md(.*?\">)");
-    string newStr = regex_replace(buffer, reg, "$1.html$2");
-    const char* newBuffer = newStr.c_str();
+    string raw = buffer;
+    raw = ConvertPath(raw.c_str());
+    raw = FormatHtml(raw.c_str());
+
+    const char* newBuffer = raw.c_str();
 
     ofstream ofileStream;
     ofileStream.open(path, ios::trunc | ios::in);
     ofileStream.write(newBuffer, strlen(newBuffer));
-    cout << "new buffer: " << newBuffer << endl;
-
+    //cout << "new buffer: " << newBuffer << endl;
     ofileStream.close();
+
+    return 0;
 }
+
 
 int main()
 {
-    const char* srcDir = "E:\\ExampleNote";
-    const char* targetDir = "E:\\Web";
-    const char* cmdFormat = "showdown makehtml -i %s -o %s";
-
+    Init();
     Updater updater = Updater();
     updater.CreateCatalog(srcDir);
 
     vector<FileInfo>* infos = updater.GetFileInfos();
+
     if (infos != NULL)
     {
+        CreateCatalogue(infos);
+
+        if (ClearTargetDirectory(targetDir) == -1)
+        {
+            return 0;
+        }
+
         for (vector<FileInfo>::const_iterator iter = infos->begin(); iter != infos->end(); ++iter)
         {
             FileInfo info = *iter;
@@ -87,18 +232,20 @@ int main()
             info.FullPath(srcFullPath);
             path srcPath(srcFullPath);
 
+            string desPathStr = srcPath.string();
+            desPathStr = desPathStr.substr(strlen(srcDir), desPathStr.length());
+            desPathStr = targetDir + desPathStr;
+            //cout << desPathStr << endl;
+            path desPath(desPathStr);
+
+            if (!exists(desPath.parent_path()))
+            {
+                create_directories(desPath.parent_path());
+            }
+
             if (srcPath.extension() == ".md")
             {
-                string desPathStr = srcPath.string();
-                desPathStr = desPathStr.substr(strlen(srcDir), desPathStr.length());
-                desPathStr = targetDir + desPathStr;
-                cout << desPathStr << endl;
-                path desPath(desPathStr);
                 desPath.replace_extension(".html");
-                if (!exists(desPath.parent_path()))
-                {
-                    create_directories(desPath.parent_path());
-                }
 
                 char cmd[300] = "";
                 snprintf(cmd, 200, cmdFormat, srcPath.string().c_str(), desPath.string().c_str());
@@ -107,10 +254,15 @@ int main()
                 char result[1024 * 4] = "";
                 if (CMDRunder(cmd, result) == 1)
                 {
-                    cout << result << endl;
+                    cout << "running : " << cmd << endl;
                 }
 
-                ConvertPath(desPath);
+                FileFactor(desPath);
+            }
+            else
+            {
+                cout << "copying file : " << desPath.filename() << endl;
+                copy(srcPath, desPath);
             }
         }
     }
